f6c5050bdf2e5e3418c11c55d92848f3d5efb0ec
ths1 <thomas.stenersen@nordicsemi.no>
2018-03-15 10:02:46 +0100

    Add setup script for pip installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pyrtt-viewer",
+    author="Thomas Stenersen",
+    author_email="stenersen.thomas@gmail.com",
+    version="0.1",
+    scripts=["pyrtt-viewer"]
+)
