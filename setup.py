17510a0c19d7860ec3a1555cf69130d2c21409b8
ths1 <thomas.stenersen@nordicsemi.no>
2018-03-20 14:36:47 +0100

    Fix device check and version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Thomas Stenersen",
     author_email="stenersen.thomas@gmail.com",
     url="https://github.com/thomasstenersen/pyrtt-viewer",
-    version="0.1",
+    version="0.1.1",
     description="A simple script for RTT I/O",
     install_requires=["pynrfjprog"],
     python_requires=">=3",
