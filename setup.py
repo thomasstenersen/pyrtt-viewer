2c99eb3fe41c697ae9fd23f8a2c5f4fda9c6c02c
ths1 <thomas.stenersen@nordicsemi.no>
2018-03-20 13:57:30 +0100

    Add url to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="pyrtt-viewer",
     author="Thomas Stenersen",
     author_email="stenersen.thomas@gmail.com",
+    url="https://github.com/thomasstenersen/pyrtt-viewer",
     version="0.1",
     scripts=["pyrtt-viewer"]
 )
