06c2aba0f4b6ee7514686bddaee514e9f87dfb36
ths1 <thomas.stenersen@nordicsemi.no>
2018-03-20 14:23:15 +0100

    Add simple description

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
+
 setup(
     name="pyrtt-viewer",
     author="Thomas Stenersen",
     author_email="stenersen.thomas@gmail.com",
     url="https://github.com/thomasstenersen/pyrtt-viewer",
     version="0.1",
+    description="A simple script for RTT I/O",
     scripts=["pyrtt-viewer"]
 )
